baa84ed853151afe3011cdf2c32bb8b15a49280c
e1525882 <e1525882@student.tuwien.ac.at>
2020-11-20 22:20:21 +0100

    fist upload

---
+++ Alex/Ex2/Distribution.hpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+template <typename T> struct Distribution {
+  using value_type = T;
+  T mean;
+  T stddev; 
+
+  Distribution(T mean, T stddev) : mean(mean), stddev(stddev){};
+
+  Distribution(std::vector<T> &dataset) : mean(0), stddev(0) {
+    std::cout << "Hallo" << std::endl;
+    // (Task 1) missing implementation which calculates
+    //  mean and stddev from 'dataset'
+  }
+
+  // (Task 2) missing functionality to 
+  // construct a Distribution from other container types
+};
---
+++ Alex/Ex2/build/CMakeFiles/test_algorithms.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_algorithms.dir/test_algorithms.cpp.o"
+  "test_algorithms.pdb"
+  "test_algorithms"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_algorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
